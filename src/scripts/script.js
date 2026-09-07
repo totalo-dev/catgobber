843f506efb01a673308dfaea1ca269aa96449c2e
Totalo <totalodev@outlook.com>
2026-09-07 17:51:14 -0300

    commit

---
+++ src/scripts/script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('spin');
+const gato = document.getElementById('gatobabudo')
+
+function girolouco() {
+    gato.classList.toggle('animate-spin')
+}
+
+btn.addEventListener('click', girolouco);
